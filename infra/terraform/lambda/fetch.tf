9245298c5b249e8d196e7ad80458e46dc1353a59
Aaditya Saxena <aaditya.saxena.1357@gmail.com>
2026-02-17 21:18:43 +0530

    minor fix in IaC

--- infra/terraform/lambda/fetch.tf
+++ infra/terraform/lambda/fetch.tf
@@ -3,7 +3,6 @@ resource "aws_lambda_function" "fetch" {
   runtime       = "python3.10"
   handler       = "handler.lambda_handler"
   role          = var.lambda_role_arn
-
   filename         = "../../backend/lambda/fetch/fetch.zip"
   source_code_hash = filebase64sha256("../../backend/lambda/fetch/fetch.zip")
 
